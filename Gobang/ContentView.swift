1779c4179749360bcbb9421ae26d4c070a020c16
YuYang Wang <yyweric0720@gmail.com>
2023-05-27 08:35:32 +0800

    Initial Commit

---
+++ Gobang/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gobang
+//
+//  Created by User06 on 2023/5/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gobang/GobangApp.swift
@@ -0,0 +1,17 @@
+//
+//  GobangApp.swift
+//  Gobang
+//
+//  Created by User06 on 2023/5/27.
+//
+
+import SwiftUI
+
+@main
+struct GobangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
